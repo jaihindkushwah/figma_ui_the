37dca2397136f0722de33ba4a4207313f454764f
jaihindkushwah <jaihindkushwaha672001@gmail.com>
2024-05-09 10:57:37 +0530

    accordion buttion fixed

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -9,7 +9,9 @@ function Accordion({ heading, children, isOpen }) {
   return (
     <span className="w-full flex flex-col pb-3 gap-4 border-b border-slate-400">
       <span className="flex justify-between p-1 text-center">
-        <span className="text-xl font-semibold">{heading}</span>
+        <span onClick={() => setOpen(!open)} className="text-xl font-semibold">
+          {heading}
+        </span>
         <span className="cursor-pointer" onClick={() => setOpen(!open)}>
           {!open ? <PlusOutlined /> : <MinusOutlined />}
         </span>
